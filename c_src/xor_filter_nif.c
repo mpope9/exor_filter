5341370d83ccbddda1d81fe0d1d0547efcfdedb9
Matthew Pope <matt.leonard.pope@gmail.com>
2019-12-22 19:26:05 -0800

    fixing inconsistent C formatting

--- c_src/xor_filter_nif.c
+++ c_src/xor_filter_nif.c
@@ -5,14 +5,16 @@
 static ErlNifResourceType* xor8_resource_type;
 static ErlNifResourceType* xor16_resource_type;
 
-typedef struct {
+typedef struct 
+{
    int         is_buffer_allocated;
    uint64_t*   buffer;
    int         is_filter_allocated;
    xor8_t*     filter;
 } xor8_filter_resource;
 
-typedef struct {
+typedef struct 
+{
    int         is_buffer_allocated;
    uint64_t*   buffer;
    int         is_filter_allocated;
@@ -23,10 +25,12 @@ void destroy_xor8_filter_resource(ErlNifEnv* env, void* obj)
 {
    xor8_filter_resource* resource = (xor8_filter_resource*) obj;
 
-   if(obj != NULL && resource->is_buffer_allocated) {
+   if(obj != NULL && resource->is_buffer_allocated) 
+   {
       enif_free(resource->buffer);
    }
-   if(obj != NULL && resource->is_filter_allocated) {
+   if(obj != NULL && resource->is_filter_allocated) 
+   {
       xor8_free(resource->filter);
       enif_free(resource->filter);
    }
@@ -36,10 +40,12 @@ void destroy_xor16_filter_resource(ErlNifEnv* env, void* obj)
 {
    xor16_filter_resource* resource = (xor16_filter_resource*) obj;
 
-   if(obj != NULL && resource->is_buffer_allocated) {
+   if(obj != NULL && resource->is_buffer_allocated) 
+   {
       enif_free(resource->buffer);
    }
-   if(obj != NULL && resource->is_filter_allocated) {
+   if(obj != NULL && resource->is_filter_allocated) 
+   {
       xor16_free(resource->filter);
       enif_free(resource->filter);
    }
@@ -103,11 +109,13 @@ xor8_initialize(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[], int buffere
       return enif_make_badarg(env);
    }
 
-   if(!enif_is_list(env, is_list)) {
+   if(!enif_is_list(env, is_list)) 
+   {
       return enif_make_badarg(env);
    }
 
-   if(!enif_get_list_length(env, argv[0], &list_length)) {
+   if(!enif_get_list_length(env, argv[0], &list_length)) 
+   {
       return mk_error(env, "get_list_length_error");
    }
 
@@ -118,13 +126,16 @@ xor8_initialize(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[], int buffere
 
    value_list = enif_alloc(sizeof(uint64_t) * list_length);
 
-   if(value_list == NULL) {
+   if(value_list == NULL) 
+   {
       return mk_error(env, "could_not_allocate_memory_error");
    }
 
    uint64_t current = 0;
-   for(int i = 0; enif_get_list_cell(env, argv[0], &head, (ERL_NIF_TERM*) &(argv[0])); i++) {
-      if(!enif_get_uint64(env, head, &current)) {
+   for(int i = 0; enif_get_list_cell(env, argv[0], &head, (ERL_NIF_TERM*) &(argv[0])); i++) 
+   {
+      if(!enif_get_uint64(env, head, &current)) 
+      {
          return mk_error(env, "convert_to_uint64_t_error");
       }
       value_list[i] = current;
@@ -135,21 +146,25 @@ xor8_initialize(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[], int buffere
    
    xor8_t* filter = enif_alloc(sizeof(xor8_t));
 
-   if(!xor8_allocate(list_length, filter)) {
+   if(!xor8_allocate(list_length, filter)) 
+   {
       enif_release_resource(filter);
       return mk_error(env, "xor8_allocate_error");
    }
 
    /* Determine to use a buffered populate for speed or not */
-   if(buffered) {
-      if(!xor8_populate(value_list, list_length, filter)) {
+   if(buffered) 
+   {
+      if(!xor8_populate(value_list, list_length, filter)) 
+      {
          enif_release_resource(filter);
          return mk_error(env, "xor8_populate_error");
       }
    }
    else
    {
-      if(!xor8_buffered_populate(value_list, list_length, filter)) {
+      if(!xor8_buffered_populate(value_list, list_length, filter)) 
+      {
          enif_release_resource(filter);
          return mk_error(env, "xor8_populate_error");
       }
@@ -196,10 +211,12 @@ xor8_contain_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
       return mk_error(env, "get_key_for_contains_error");
    }
 
-   if(xor8_contain(key, filter)) {
+   if(xor8_contain(key, filter)) 
+   {
       return mk_atom(env, "true");
    }
-   else {
+   else 
+   {
       return mk_atom(env, "false");
    }
 
@@ -216,7 +233,8 @@ xor8_free_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
    }
 
    xor8_filter_resource* filter_resource;
-   if(!enif_get_resource(env, argv[0], xor8_resource_type, (void**) &filter_resource)) {
+   if(!enif_get_resource(env, argv[0], xor8_resource_type, (void**) &filter_resource)) 
+   {
       return mk_error(env, "get_filter_for_deallocation_error");
    }
 
@@ -239,11 +257,13 @@ xor16_initialize(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[], int buffer
       return enif_make_badarg(env);
    }
 
-   if(!enif_is_list(env, is_list)) {
+   if(!enif_is_list(env, is_list)) 
+   {
       return enif_make_badarg(env);
    }
 
-   if(!enif_get_list_length(env, argv[0], &list_length)) {
+   if(!enif_get_list_length(env, argv[0], &list_length)) 
+   {
       return mk_error(env, "get_list_length_error");
    }
 
@@ -254,13 +274,16 @@ xor16_initialize(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[], int buffer
 
    value_list = enif_alloc(sizeof(uint64_t) * list_length);
 
-   if(value_list == NULL) {
+   if(value_list == NULL) 
+   {
       return mk_error(env, "could_not_allocate_memory_error");
    }
 
    uint64_t current = 0;
-   for(int i = 0; enif_get_list_cell(env, argv[0], &head, (ERL_NIF_TERM*) &(argv[0])); i++) {
-      if(!enif_get_uint64(env, head, &current)) {
+   for(int i = 0; enif_get_list_cell(env, argv[0], &head, (ERL_NIF_TERM*) &(argv[0])); i++) 
+   {
+      if(!enif_get_uint64(env, head, &current)) 
+      {
          return mk_error(env, "convert_to_uint64_t_error");
       }
       value_list[i] = current;
@@ -271,21 +294,25 @@ xor16_initialize(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[], int buffer
    
    xor16_t* filter = enif_alloc(sizeof(xor8_t));
 
-   if(!xor16_allocate(list_length, filter)) {
+   if(!xor16_allocate(list_length, filter)) 
+   {
       enif_release_resource(filter);
       return mk_error(env, "xor16_allocate_error");
    }
 
    /* Determine to use a buffered populate for speed or not */
-   if(buffered) {
-      if(!xor16_populate(value_list, list_length, filter)) {
+   if(buffered) 
+   {
+      if(!xor16_populate(value_list, list_length, filter)) 
+      {
          enif_release_resource(filter);
          return mk_error(env, "xor16_populate_error");
       }
    }
    else
    {
-      if(!xor16_buffered_populate(value_list, list_length, filter)) {
+      if(!xor16_buffered_populate(value_list, list_length, filter)) 
+      {
          enif_release_resource(filter);
          return mk_error(env, "xor16_populate_error");
       }
@@ -320,20 +347,24 @@ xor16_contain_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
    }
 
    xor16_filter_resource* filter_resource;
-   if(!enif_get_resource(env, argv[0], xor16_resource_type, (void**) &filter_resource)) {
+   if(!enif_get_resource(env, argv[0], xor16_resource_type, (void**) &filter_resource)) 
+   {
       return mk_error(env, "get_filter_for_contains_error");
    }
    xor16_t* filter = filter_resource->filter;
 
    ErlNifUInt64 key;
-   if(!enif_get_uint64(env, argv[1], &key)) {
+   if(!enif_get_uint64(env, argv[1], &key)) 
+   {
       return mk_error(env, "get_key_for_contains_error");
    }
 
-   if(xor16_contain(key, filter)) {
+   if(xor16_contain(key, filter)) 
+   {
       return mk_atom(env, "true");
    }
-   else {
+   else 
+   {
       return mk_atom(env, "false");
    }
 
@@ -350,7 +381,8 @@ xor16_free_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
    }
 
    xor16_filter_resource* filter_resource;
-   if(!enif_get_resource(env, argv[0], xor16_resource_type, (void**) &filter_resource)) {
+   if(!enif_get_resource(env, argv[0], xor16_resource_type, (void**) &filter_resource)) 
+   {
       return mk_error(env, "get_filter_for_deallocation_error");
    }
 
